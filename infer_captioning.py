e4ab29a9a320fe42e8ab81dab758ebfec6e54b24
Bui Thanh Xuan <buithanhxuan2261@gmail.com>
2024-12-09 22:20:02 +0700

    Add files via upload
    unvg

--- infer_captioning.py
+++ infer_captioning.py
@@ -7,7 +7,6 @@
 
 import os
 import sys
-import json
 import logging
 
 pth = '/'.join(sys.path[0].split('/')[:-1])
@@ -15,22 +14,21 @@ sys.path.insert(0, pth)
 
 from PIL import Image
 import numpy as np
-np.random.seed(27)
+np.random.seed(0)
+import cv2
 
 import torch
 from torchvision import transforms
 
 from utils.arguments import load_opt_command
-
 from detectron2.data import MetadataCatalog
-from detectron2.utils.colormap import random_color
-from detectron2.data.datasets.builtin_meta import COCO_CATEGORIES
+from detectron2.structures import BitMasks
 from modeling.BaseModel import BaseModel
 from modeling import build_model
+from detectron2.utils.colormap import random_color
 from utils.visualizer import Visualizer
 from utils.distributed import init_distributed
 
-# logging.basicConfig(level = logging.INFO)
 logger = logging.getLogger(__name__)
 
 
@@ -38,32 +36,29 @@ def main(args=None):
     '''
     Main execution point for PyLearn.
     '''
+    
     opt, cmdline_args = load_opt_command(args)
     if cmdline_args.user_dir:
         absolute_user_dir = os.path.abspath(cmdline_args.user_dir)
         opt['base_path'] = absolute_user_dir
-
     opt = init_distributed(opt)
 
     # META DATA
     pretrained_pth = os.path.join(opt['RESUME_FROM'])
+    if 'novg' not in pretrained_pth:
+        assert False, "Using the ckpt without visual genome training data will be much better."
     output_root = '/kaggle/working/'
-    image_pth = 'inference/images/fruit.jpg'
-
-    text = [['The larger watermelon.'], ['The front white flower.'], ['White tea pot.'], ['Flower bunch.'], ['white vase.'], ['The left peach.'], ['The brown knife.']]
+    image_pth = 'inference/images/mountain.jpeg'
 
     model = BaseModel(opt, build_model(opt)).from_pretrained(pretrained_pth).eval().cuda()
-    model.model.sem_seg_head.predictor.lang_encoder.get_text_embeddings(["background", "background"], is_eval=False)
+    model.model.sem_seg_head.predictor.lang_encoder.get_text_embeddings(["background"], is_eval=False)
 
     t = []
-    t.append(transforms.Resize(512, interpolation=Image.BICUBIC))
+    t.append(transforms.Resize(224, interpolation=Image.BICUBIC))
     transform = transforms.Compose(t)
 
-    metadata = MetadataCatalog.get('ade20k_panoptic_train')
-    model.model.metadata = metadata
-
     with torch.no_grad():
-        image_ori = Image.open(image_pth)
+        image_ori = Image.open(image_pth).convert("RGB")
         width = image_ori.size[0]
         height = image_ori.size[1]
         image = transform(image_ori)
@@ -71,18 +66,29 @@ def main(args=None):
         image_ori = np.asarray(image_ori)
         images = torch.from_numpy(image.copy()).permute(2,0,1).cuda()
 
-        batch_inputs = [{'image': images, 'height': height, 'width': width, 'groundings': {'texts': text}}]
-        outputs = model.model.evaluate_grounding(batch_inputs, None)
-        visual = Visualizer(image_ori, metadata=metadata)
+        batch_inputs = [{'image': images, 'height': height, 'width': width, 'image_id': 0}]
+        outputs = model.model.evaluate_captioning(batch_inputs)
+        text = outputs[-1]['captioning_text']
 
-        grd_mask = (outputs[0]['grounding_mask'] > 0).float().cpu().numpy()
-        for idx, mask in enumerate(grd_mask):
-            demo = visual.draw_binary_mask(mask, color=random_color(rgb=True, maximum=1).astype(np.int64).tolist(), text=text[idx], alpha=0.3)
+        image_ori = image_ori[:,:,::-1].copy()
+        cv2.rectangle(image_ori, (0, 0), (width, 60), (0,0,0), -1)
+        font                   = cv2.FONT_HERSHEY_DUPLEX
+        fontScale              = 1.2
+        thickness              = 2
+        lineType               = 2
+        bottomLeftCornerOfText = (10, 40)
+        fontColor              = [255,255,255]
+        cv2.putText(image_ori, text,
+            bottomLeftCornerOfText,
+            font, 
+            fontScale,
+            fontColor,
+            thickness,
+            lineType)
 
-        output_folder = os.path.join(os.path.join(output_root))
-        if not os.path.exists(output_folder):
-            os.makedirs(output_folder)
-        demo.save(os.path.join(output_folder, 'refseg.png'))
+        if not os.path.exists(output_root):
+            os.makedirs(output_root)
+        cv2.imwrite(os.path.join(output_root, 'captioning.png'), image_ori)
 
 
 if __name__ == "__main__":
--- infer_semseg.py
+++ infer_semseg.py
@@ -7,7 +7,6 @@
 
 import os
 import sys
-import json
 import logging
 
 pth = '/'.join(sys.path[0].split('/')[:-1])
@@ -15,7 +14,7 @@ sys.path.insert(0, pth)
 
 from PIL import Image
 import numpy as np
-np.random.seed(27)
+np.random.seed(1)
 
 import torch
 from torchvision import transforms
@@ -24,13 +23,11 @@ from utils.arguments import load_opt_command
 
 from detectron2.data import MetadataCatalog
 from detectron2.utils.colormap import random_color
-from detectron2.data.datasets.builtin_meta import COCO_CATEGORIES
 from modeling.BaseModel import BaseModel
 from modeling import build_model
 from utils.visualizer import Visualizer
 from utils.distributed import init_distributed
 
-# logging.basicConfig(level = logging.INFO)
 logger = logging.getLogger(__name__)
 
 
@@ -42,28 +39,35 @@ def main(args=None):
     if cmdline_args.user_dir:
         absolute_user_dir = os.path.abspath(cmdline_args.user_dir)
         opt['base_path'] = absolute_user_dir
-
     opt = init_distributed(opt)
 
     # META DATA
     pretrained_pth = os.path.join(opt['RESUME_FROM'])
     output_root = '/kaggle/working/'
-    image_pth = 'inference/images/fruit.jpg'
-
-    text = [['The larger watermelon.'], ['The front white flower.'], ['White tea pot.'], ['Flower bunch.'], ['white vase.'], ['The left peach.'], ['The brown knife.']]
+    image_pth = 'inference/images/animals.png'
 
     model = BaseModel(opt, build_model(opt)).from_pretrained(pretrained_pth).eval().cuda()
-    model.model.sem_seg_head.predictor.lang_encoder.get_text_embeddings(["background", "background"], is_eval=False)
 
     t = []
     t.append(transforms.Resize(512, interpolation=Image.BICUBIC))
     transform = transforms.Compose(t)
 
-    metadata = MetadataCatalog.get('ade20k_panoptic_train')
+    stuff_classes = ['zebra','antelope','giraffe','ostrich','sky','water','grass','sand','tree']
+    stuff_colors = [random_color(rgb=True, maximum=255).astype(np.int).tolist() for _ in range(len(stuff_classes))]
+    stuff_dataset_id_to_contiguous_id = {x:x for x in range(len(stuff_classes))}
+
+    MetadataCatalog.get("demo").set(
+        stuff_colors=stuff_colors,
+        stuff_classes=stuff_classes,
+        stuff_dataset_id_to_contiguous_id=stuff_dataset_id_to_contiguous_id,
+    )
+    model.model.sem_seg_head.predictor.lang_encoder.get_text_embeddings(stuff_classes + ["background"], is_eval=True)
+    metadata = MetadataCatalog.get('demo')
     model.model.metadata = metadata
+    model.model.sem_seg_head.num_classes = len(stuff_classes)
 
     with torch.no_grad():
-        image_ori = Image.open(image_pth)
+        image_ori = Image.open(image_pth).convert("RGB")
         width = image_ori.size[0]
         height = image_ori.size[1]
         image = transform(image_ori)
@@ -71,18 +75,16 @@ def main(args=None):
         image_ori = np.asarray(image_ori)
         images = torch.from_numpy(image.copy()).permute(2,0,1).cuda()
 
-        batch_inputs = [{'image': images, 'height': height, 'width': width, 'groundings': {'texts': text}}]
-        outputs = model.model.evaluate_grounding(batch_inputs, None)
+        batch_inputs = [{'image': images, 'height': height, 'width': width}]
+        outputs = model.forward(batch_inputs)
         visual = Visualizer(image_ori, metadata=metadata)
 
-        grd_mask = (outputs[0]['grounding_mask'] > 0).float().cpu().numpy()
-        for idx, mask in enumerate(grd_mask):
-            demo = visual.draw_binary_mask(mask, color=random_color(rgb=True, maximum=1).astype(np.int64).tolist(), text=text[idx], alpha=0.3)
+        sem_seg = outputs[-1]['sem_seg'].max(0)[1]
+        demo = visual.draw_sem_seg(sem_seg.cpu(), alpha=0.5) # rgb Image
 
-        output_folder = os.path.join(os.path.join(output_root))
-        if not os.path.exists(output_folder):
-            os.makedirs(output_folder)
-        demo.save(os.path.join(output_folder, 'refseg.png'))
+        if not os.path.exists(output_root):
+            os.makedirs(output_root)
+        demo.save(os.path.join(output_root, 'sem.png'))
 
 
 if __name__ == "__main__":
